ffb03df7898cb2a08dc501d8b077be88d0f15cfc
Elizabeth Solis Graniel <elizabeth.solisgraniel@gmail.com>
2024-12-07 22:11:11 +0100

    Primer commit

---
+++ pupilas.gd
@@ -0,0 +1,21 @@
+extends Node2D  # Asegúrate de que el nodo 'pupilas' sea un Node2D o similar
+
+# Constante para la distancia máxima de movimiento de las pupilas
+const MAX_DISTANCE = 3
+
+func _process(delta):
+	# Obtener la posición global del ratón
+	var mouse_pos = get_global_mouse_position()
+	
+	# Obtener la posición global del centro del ojo (padre del nodo actual)
+	var eye_center = get_parent().global_position
+
+	# Calcular el vector de dirección del centro del ojo al ratón
+	var direction = (mouse_pos - eye_center).normalized()
+
+	# Limitar el movimiento de las pupilas
+	var distance = (mouse_pos - eye_center).length()
+	var constrained_pos = direction * min(MAX_DISTANCE, distance)
+
+	# Actualizar la posición de las pupilas relativa al nodo padre
+	position = constrained_pos
